9497da736a98284bf2e998cb749ee65261eda937
刘雨飏 <ryanemax@icloud.com>
2017-07-27 12:48:09 +0800

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Ng Lazyload starter';
+  title = 'Ng Lazyload Starter';
 }
